5f3f5f514b1eb7a9b55ee43584dac9b39e01363b
souravguha09 <souravguha2025@gmail.com>
2025-08-13 22:40:47 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "saiyaara": "https://www.youtube.com/watch?v=BSJa1UytM8w",
+    "Aaj ki raat": "https://www.youtube.com/watch?v=hxMNYkLN7tI",
+    "Nasha": "https://www.youtube.com/watch?v=NZ1EBaqDL0M",
+}
